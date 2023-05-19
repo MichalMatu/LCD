7d29ee8f9fef754436733a4aac46ed5b7d67d2b4
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-19 04:30:34 +0200

    lcd

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,21 @@ const int i2cAddress = 0x27;
 // Create a new instance of the LiquidCrystal_PCF8574 class
 LiquidCrystal_PCF8574 lcd(i2cAddress);
 
-void setup() {
+void setup()
+{
   // Initialize the LCD screen
-  lcd.begin(16, 2);
-
-  // Print a message to the LCD screen
-  lcd.print("Hello, world!");
+  lcd.begin(16, 4);
 }
 
-void loop() {
-  // Do nothing
+void loop()
+{
+  // Display "Hello World!" on each line of the screen
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World!");
+  lcd.setCursor(0, 4);
+  lcd.print("Hello World!");
+  lcd.setCursor(0, 6);
+  lcd.print("Hello World!");
+
+  delay(1000);
 }
